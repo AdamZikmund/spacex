81cc1e4de24d84694fb7d394a2c0d45f198db833
Adam Zikmund <adamzikmund289@gmail.com>
2023-06-07 14:03:49 +0200

    Implemented MainApp for showing pure SwiftUI flow
    Minor fix

---
+++ spacex/MainApp.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+// @main
+struct MainApp: App {
+    // MARK: - Properties
+    private let service: Service
+
+    // MARK: - Lifecycle
+    init() {
+#if LIVE
+        self.service = LiveService.build()
+#else
+        self.service = MockService.build()
+#endif
+    }
+
+    // MARK: - Body
+    var body: some Scene {
+        WindowGroup {
+            MainFlowView(
+                viewModel: .init(service: service)
+            )
+        }
+    }
+}
--- spacex/ViewComponent/Sources/ViewComponent/LaunchCell.swift
+++ spacex/ViewComponent/Sources/ViewComponent/LaunchCell.swift
@@ -43,6 +43,7 @@ public struct LaunchCell: View {
                 Divider()
             }
         }
+        .contentShape(Rectangle())
     }
 }
 
